e28d8e30b60ba63b06a0827d7c7c15e092a40ab2
Mohammed Ali <binbasri.india@gmail.com>
2025-10-30 15:16:06 +0000

    create new file for pytorch version of the training

---
+++ src/nsbi_common_utils/training_pytorch.py
@@ -0,0 +1,15 @@
+class TrainEvaluate_Pytorch_NN:
+    def __init__(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def predict_with_model(self):
+        pass
+    
+    def make_overfit_plots(self):
+        pass
+    
+def build_model_pytorch():
+    pass
