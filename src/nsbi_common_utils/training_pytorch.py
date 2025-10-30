371a8f813678c191dbf6f7b4360eb733ee5b3910
Mohammed Ali <binbasri.india@gmail.com>
2025-10-30 07:27:49 +0000

    created new file with converting base model to pytorch version

---
+++ src/nsbi_common_utils/training_pytorch.py
@@ -0,0 +1,17 @@
+# replacing tensorflow with pytorch
+class TrainEvaluate_Pytorch_NN:
+    def __init__(self):
+        pass
+    
+    def train(self):
+        pass
+    
+    def predict_with_model(self):
+        pass
+    
+    def make_overfit_plots(self):
+        pass
+    
+    
+def build_model_pytorch():
+    pass
